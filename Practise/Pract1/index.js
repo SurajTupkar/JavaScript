119ca12552b0ecd17daf7c929702ad1e9fddeada
Suraj Tupkar <98230459+SurajTupkar@users.noreply.github.com>
2024-11-13 16:06:05 +0000

    practise

---
+++ Practise/Pract1/index.js
@@ -0,0 +1,9 @@
+/*There 2 main categories of datatypes
+    1) Primitive 
+    2) Reference
+These two categories are on the basis of call by value and call by reference it means how we store data and we fetch data.
+
+In primitive data types there are 7 datatypes
+
+
+*/
